ddc7c6e41641b40dd92ea8ad710a62d7b7c8dd66
Gragit <gme2468@gmail.com>
2020-09-16 13:31:28 +0800

    timer working

---
+++ script.js
@@ -0,0 +1,23 @@
+var countdownEl = document.getElementById("timer");
+
+var minsLeft = 5
+var secondsLeft = minsLeft * 60;
+var timeDisplay = "Timer: " + minsLeft +": " + secondsLeft
+
+function countDown() {
+    setInterval(function () {
+        countdownEl.textContent = secondsLeft
+        secondsLeft--;
+
+        if (secondsLeft <= 0) {
+            clearInterval(secondsLeft = 0);
+        }
+
+    }, 1000);
+
+    console.log("timer started")
+}
+
+
+countDown();
+
